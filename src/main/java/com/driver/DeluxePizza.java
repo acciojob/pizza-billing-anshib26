bfc1e66ccfc8206b128d17ff885f836a44610872
anshib26 <84781158+anshib26@users.noreply.github.com>
2024-05-05 05:22:38 +0530

    Second Submission

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,11 +4,13 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        if(isVeg){
-            setPrice(150);
-        }
-        else{
-            setPrice(200);
-        }
+        addExtraCheese();
+        addExtraToppings();
+//        if(isVeg){
+//            setPrice(150);
+//        }
+//        else{
+//            setPrice(200);
+//        }
     }
 }
